1dc13538b16ea8b7e3b8ef75874410e90bf488c7
yechan kim <rladPcks980@gmail.com>
2022-07-26 23:19:29 +0900

    6

--- index.js
+++ index.js
@@ -1252,10 +1252,415 @@ console.log(Object.getOwnPropertyDescriptors(person));
 
 
 
-  // <<접근자 프로퍼티>> 16.3.2
-  // 접근자 프로퍼티는 자체적으로는 값을 갖지 않고 다른 데이터 프로퍼티의 값을 읽거나 저장할 때 사용하는 접근 함수로 구성된 프로퍼티다.
+// <<접근자 프로퍼티>> 16.3.2
+// 접근자 프로퍼티는 자체적으로는 값을 갖지 않고 다른 데이터 프로퍼티의 값을 읽거나 저장할 때 사용하는 접근 함수로 구성된 프로퍼티다.
+// 접근자 함수는 getter / setter 함수라고도 부른다. 접근자 프로퍼티는 getter와 setter 함수를 모두 정의할 수도 있고 하나만 정의할 수도 있다.
+const person = {
+    // 데이터 프로퍼티
+    firstName: 'Ungmo',
+    lastName: 'Lee',
+
+    // fullName은 접근자 함수로 구성된 접근자 프로퍼티다.
+    // getter 함수
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    // setter 함수
+    set fullName(name) {
+        // 배열 디스트럭처링 할당: "31.1 배열 디스트럭처링 할당" 참고
+        [this.firstName, this.lastName] = name.split(' ');
+    }
+};
+
+// 데이터 프로퍼티를 통한 프로퍼티 값의 참조.
+console.log(person.firstName+ ' ' + person.lastName);  // Ungmo Lee
+
+// 접근자 프로퍼티를 통한 프로퍼티 값의 저장
+// 접근자 프로퍼티 fullName에 값을 저장하면 setter 함수가 호출된다.
+person.fullName = 'Heegon Lee';
+console.log(person);      // { firstName: 'Heegon', lastName: 'Lee', fullName: [Getter/Setter] }
+
+// 접근자 프로퍼티를 통한 프로퍼티 값의 참조
+// 접근자 프로퍼티 fullName에 접근하면 getter 함수가 호출된다.
+console.log(person.fullName);   // Heegon Lee
+
+// firstName은 데이터 프로퍼티다
+//let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName');
+console.log(descriptor);  
+// {value: 'Heegon', writable: true, enumerable: true, configurable: true}
+
+// fullName은 접근자 프로퍼티다.
+descriptor = Object.getOwnPropertyDescriptor(person, 'fullName');
+console.log(descriptor);
+// {get: [Function: get fullName], set: [Function: set fullName], enumerable: true, configurable: true}
+
+
+
+// <<접근자 프로퍼티와 데이터 프로퍼티를 구별하는 방법>>
+// 일반 객체의 __proto__ 는 접근자 프로퍼티다.
+Object.getOwnPropertyDescriptor(Object.prototype, '__proto__');
+// {get: f, set: f, enumerable: false, configurable: true}
+
+// 함수 객체의 prototype은 데이터 프로퍼티다.
+Object.getOwnPropertyDescriptor(function() {}, 'prototype');
+// {value: {...}, writable: true, enumerable: false, configurable: false}
+
+
+
+// <<<프로퍼티 정의>>> 16.4
+// Object.defineProperty 메서드를 사용하면 프로퍼티의 어트리뷰트를 정의할 수 있다. 인수로는 객체의 참조와 데이터 프로퍼티의 키인 문자열, 프로퍼티 디스크립터 객체를 전달한다.
+const person = {};
+
+// 데이터 프로퍼티 정의
+Object.defineProperty(person, 'firstName',{
+    value: 'Ungmo',
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty(person, 'lastName', {
+    value: 'Lee'
+});
+
+let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName');
+console.log('firstName', descriptor);
+// firstName {value: 'Ungmo', writable: true, enumerable: true, configurable: true}
+
+// 디스크립터 객체의 프로퍼티를 누락시키면 undefined, false가 기본값이다.
+descriptor = Object.getOwnPropertyDescriptor(person, 'lastName');
+console.log('lastName', descriptor);
+// lastName {value: 'Lee', writable: false, enumerable: false, configurable: false}
+
+// [[Enumerable]]의 값이 false인 경우
+// 해당 프로퍼티는 for...in 문이나 Object.keys 등으로 열거할 수 없다.
+// lastName 프로퍼티는 [[Enumerable]]의 값이 false이므로 열거되지 않는다.
+console.log(Object.keys(person)); // ["firstName"]
+
+// [[Writable]]의 값이 false인 경우 해당 프로퍼티의 [[Value]]의 값을 변경할 수 없다.
+// lastName 프로퍼티는 [[Writable]]의 값이 false이므로 값을 변경할 수 없다.
+// 이때 값을 변경하면 에러는 발생하지 않고 무시된다.
+person.lastname = 'Kim';
+
+// [[Configurable]]의 값이 false인 경우 해당 프로퍼티를 삭제할 수 없다.
+// lastName 프로퍼티는 [[Configurable]]의 값이 false이므로 삭제할 수 없다.
+// 이때 프로퍼티를 삭제하면 에러는 발생하지 않고 무시된다.
+delete person.lastName;
+
+// [[Configurable]]의 값이 false인 경우 해당 프로퍼티를 재정의할 수 없다.
+// Object.defindeProperty(person, 'lastName', { enumerable: true});
+// Uncaught TypeError: Cannot redefine property: lastName
+
+descriptor = Object.getOwnPropertyDescriptor(person, 'lastName');
+console.log('lastName', descriptor);
+//lastName {value: 'Lee', writable: false, enumerable: false, configurable: false}
+
+// 접근자 프로퍼티 정의
+Object.defineProperty(person, 'fullName', {
+    // getter 함수
+    get() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    // setter 함수
+    set(name) {
+        [this.firstName, this.lastName] = name.split(' ');
+    },
+    enumerable: true,
+    configurable: true
+});
+
+descriptor = Object.getOwnPropertyDescriptor(person, 'fullName');
+console.log('fullName', descriptor);
+// fullName {get: [Function: get], set: [Function: set], enumerable: true, configurable: true}
+
+person.fullName = 'Heegun Lee';
+console.log(person); // { firstName: 'Heegun', lastname: 'Kim', fullName: [Getter/Setter] }
+
+
+
+
+// Object.defineProperty 메서드는 한번에 하나의 프로퍼티만 정의할 수 있지만 Object.defineProperties 메서드를 사용하면 여래 개의 프로퍼티를 한 번에 정의할 수 있다.
+const person = {};
+
+Object.defineProperties(person, {
+    firstName: {
+        value: 'Ungmo',
+        writable: true,
+        enumerable: true,
+        configurable: true
+    },
+    lastName: {
+        value: 'Lee',
+        writable: true,
+        enumerable: true,
+        configurable: true
+    },
+    // 접근자 프로퍼티 정의
+    fullName: {
+        //getter 함수
+        get() {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        // setter 함수
+        set(name) {
+            [this.firstName, this.lastName] = name.split(' ');
+        },
+        enumerable: true,
+        configurable: true
+    }
+});
+person.fullName = 'Heegon Lee';
+console.log(person); // { firstName: 'Heegon', lastName: 'Lee', fullName: [Getter/Setter] }
+
+
+
+// <<<객체 변경 방지>>> 16.5
+// <<객체 확장 금지>> 16.5.1
+// Object.perventExtensions 메서드는 객체의 확장을 금지한다. 객체 확장 금지란 프로퍼티 추가 금지를 의미한다. 즉, 확장이 금지된 객체는 프로퍼티 추가가 금지된다. 프로퍼티는 프로퍼티 동적 추가와 Object.defineProperty 메서드로 추가할 수 있다. 이 두 가지 추가 방법이 모두 금지된다.
+// 확장이 가능한 객체인지 여부는 Object.isExtensible 메서드로 확인할 수 있다.
+const person = {name: 'Lee'};
+
+// person 객체는 확장이 금지된 객체가 아니다.
+console.log(Object.isExtensible(person));
+
+// person 객체의 확장을 금지하여 프로퍼티 추가를 금지한다.
+Object.preventExtensions(person);
+
+// person 객체는 확장이 금지된 객체다.
+console.log(Object.isExtensible(person));
+
+// 프로퍼티 추가가 금지된다.
+person.age = 20; // 무시. strict mode 에서는 에러
+console.log(person); // {name: 'Lee'}
+
+// 프로퍼티 추가는 금지되지만 삭제는 가능하다.
+delete person.name;
+console.log(person); // {}
+
+// 프로퍼티 정의에 의한 프로퍼티 추가도 금지된다.
+Object.defineProperty(person, 'age', {value: 20});
+// TypeError: Cannot define property age, object is not extensible 
+
+
+
+// <<객체 밀봉>> 16.5.2
+// Object.seal 메서드는 객체를 밀봉한다. 객체 밀봉이란 프로퍼티 추가 및 삭제와 프로퍼티 어트리뷰트 재정의 금지를 의미한다. 즉, 밀봉된 객체는 읽기와 쓰기만 가능하다.
+// 밀봉된 객체인지 여부는 Object.isSealed 메서드로 확인할 수 있다.
+const person = {name: 'Lee'};
+
+// person 객체는 밀봉(seal)된 객체가 아니다.
+console.log(Object.isSealed(person)); // false
+
+// person 객체를 밀봉(seal)하여 프로퍼티 추가, 삭제, 재정의를 금지한다.
+Object.seal(person);
+
+// person 객체는 밀봉(seal)된 객체다.
+console.log(Object.isSealed(person)); // true
+
+// 밀봉된 객체는 configurable이 false다.
+let descriptor = Object.getOwnPropertyDescriptor(person , 'name');
+console.log('name', descriptor);
+// name { value: 'Lee', writable: true, enumerable: true, configurable: false }
+
+// 프로퍼티 추가가 금지된다.
+person.age = 20;       // 무시. strict mode에서는 에러
+console.log(person);   // {name: 'Lee'}
+
+// 프로퍼티 삭제가 금지된다.
+delete person.name; // 무시. strict mode에서는 에러
+console.log(person);  // {name: 'Lee'}
+
+// 프로퍼티 값 갱신은 가능하다.
+person.name = 'Kim';
+console.log(person);  // {name: 'Kim'}
+
+// 프로퍼티 어트리뷰트 재정의가 금지된다.
+Object.defineProperty(person, 'name' , { configurable: true });
+// TypeError: Cannot redefine property: name
+
+
+
+// <<객체 동결>> 16.5.3
+// Object.freeze 메서드는 객체를 동결한다. 객체 동결이란 프로퍼티 추가 및 삭제와 프로퍼티 어트리뷰트 재정의 금지, 프로퍼티 값 갱신 금지를 의미한다. 즉, 동결된 객체는 읽기만 가능하다.
+// 동결된 객체인지 여부는 Object.isFrozen 메서드로 확인할 수 있다.
+const person = { name: 'Lee' };
+
+// person 객체는 동결된 객체가 아니다.
+console.log(Object.isFrozen(person)); // false
+
+// person 객체를 동결하여 프로퍼티 추가, 삭제, 재정의, 쓰기를 금지한다.
+Object.freeze(person);
+
+// person 객체는 동결된 객체다
+console.log(Object.isFrozen(person)); // true
+
+let descriptor = Object.getOwnPropertyDescriptor(person, 'name');
+console.log('name', descriptor);
+//name { value: 'Lee', writable: false, enumerable: true, configurable: false }
+
+// 프로퍼티 추가가 금지된다.
+person.age = 20;   // 무시. strict mode에서는 에러
+console.log(person);   // {name: 'Lee'}
+
+// 프로퍼티 값 갱신이 금지된다.
+person.name = 'Kim'; // 무시. strict mode에서는 에러
+console.log(person); // {name: 'Lee'}
+
+// 프로퍼티 어트리뷰트 재정의가 금지된다.
+Object.defineProperty(person, 'name', { configurable: true });
+// TypeError: Cannot redefine property: name
+
+
+
+// <<불변 객체>> 16.5.4
+// 지금까지 살펴본 변경 방지 메서드들은 얕은 병경 방지로 직속 프로퍼티만 변경이 방지되고 중첩 객체까지는 영향을 주지는 못한다. 따라서 Object.freeze 메서드로 객체를 동결하여도 중첩 객체까지 동결할 수 없다.
+const person = {
+    name: 'Lee',
+    address: { city: 'Seoul' }
+};
+
+// 얕은 객체 동결
+Object.freeze(person);
+
+// 직속 프로퍼티만 동결한다.
+console.log(Object.isFrozen(person)); // true
+// 중첩 객체까지 동결하지 못한다.
+console.log(Object.isFrozen(person.address)); // false
+
+person.address.city = 'Busan';
+console.log(person);  // { name: 'Lee', address: { city: 'Busan' } }
 
 
 
 
 
+// 객체의 중첩 객체까지 동결하여 변경이 불가능한 읽기 전용의 불변 객체를 구현하려면 객체를 값으로 갖는 모든 프로퍼티에 대해 재귀적으로 Object.freeze 메서드를 호출해야 한다.
+function deepFreeze(target) {
+    // 객체가 아니거나 동결된 객체는 무시하고 객체이고 동결되지 않은 객체만 동결한다.
+    if (target && typeof target === 'object' && !Object.isFrozen(target)) {
+        Object.freeze(target);
+        /* 모든 프로퍼티를 순회하며 재귀적으로 동결한다.
+        Object.keys 메서드는 객체 자신의 열거 가능한 프로퍼티 키를 배열로 반환한다.
+        forEach 메서드는 배열을 순회하며 배열의 각 요소에 대하여 콜백 함수를 실행한다.
+        */
+       Object.keys(target).forEach(key => deepFreeze(target[key]));
+    }
+    return target;
+}
+
+const person = {
+    name: 'Lee',
+    address: { city: 'Seooul'}
+};
+
+// 깊은 객체 동결
+deepFreeze(person);
+
+console.log(Object.isFrozen(person)); // true
+//중첩 객체까지 동결한다.
+console.log(Object.isFrozen(person.address)); // true
+
+person.address.city = 'Busan';
+console.log(person); // {name: 'Lee', address: {city: 'Seoul'}}
+
+
+
+
+// <<<<생성자 함수에 의한 객체 생성>>>> 17장
+// <<<Object 생성자 함수>>> 17.1
+// new 연산자와 함께 Object 생성자 함수를 호출하면 빈 객체를 생성하여 반환한다. 빈 객체를 생성한 이후 프로퍼티 또는 메서드를 추가하여 객체를 완성할 수 있다.
+
+// 빈 객체의 생성
+const person = new Object();
+
+// 프로퍼티 추가
+person.name = 'Lee';
+person.sayHello = function() {
+    console.log('Hi! My name is' + this.name);
+};
+
+console.log(person); // { name: 'Lee', sayHello: [Function (anonymous)] }
+person.sayHello();   // Hi! My name isLee
+
+
+
+// 생성자 함수란 new 연산자와 함께 호출하여 객체(인스턴스)를 생성하는 함수를 말한다. 생성자 함수에 의해 생성된 객체를 인스턴스라 한다.
+// 자바스크립는 Object 생성자 함수 이외에도 String, Number, Boolean, Function, Array, Date, RegExp, Promise 등의 빌트인 생성자 함수를 제공한다.
+
+// String 생성자 함수에 의한 String 객체 생성
+const strObj = new String('Lee');
+console.log(typeof strObj);  // object
+console.log(strObj);         // [String: 'Lee']
+
+// Number 생성자 함수에 의한 Number 객체 생성
+const numObj = new Number(123);
+console.log(typeof numObj);  // object
+console.log(numObj);         // [Number: 123]
+
+// Boolean 생성자 함수에 의한 Boolean 객체 생성
+const boolObj = new Boolean(true);
+console.log(typeof boolObj); // object
+console.log(boolObj);        // [Boolean: true]
+
+// Function 생성자 함수에 의한 Function 객체(함수) 생성
+const func = new Function('x' ,'return x * x');
+console.log(typeof func);    // function
+console.dir(func);           // [Function: anonymous]
+
+// Array 생성자 함수에 의한 Array 객체(배열)생성
+const arr = new Array(1,2,3);
+console.log(typeof arr);     // object
+console.log(arr);            // [ 1, 2, 3 ]
+
+// RegExp 생성자 함수에 의한 RegExp 객체(정규 표현식) 생성
+const regExp = new RegExp(/ab+c/i); 
+console.log(typeof regExp);  // object
+console.log(regExp);         // /ab+c/i
+
+// Date 생성자 함수에 의한 Date 객체 생성
+const date = new Date();
+console.log(typeof date);    // object
+console.log(date);           // 2022-07-26T14:01:18.117Z
+
+
+
+
+// <<<생성자 함수>>> 17.2
+// <<객체 리터럴에 의한 객체 생성 방식의 문제점>> 17.2.1
+// 객체 리터럴에 의한 객체 생성 방식은 직관적이고 간편하다. 하지만 객체 리터럴에 의한 객체 생성 방식은 단 하나의 객체만 생성한다. 따라서 동일한 프로퍼티를 갖는 객체를 여러 개 생성해야 하는 경우 매번 같은 프로퍼티를 기술해야 하기 때문에 비효율적이다.
+const circle1 = {
+    radius: 5,
+    getDiamneter() {
+        return 2 * this.radius;
+    }
+};
+console.log(circle1.getDiamneter());   // 10
+
+const circle2 = {
+    radius: 10,
+    getDiamneter() {
+        return 2 * this.radius;
+    }
+};
+console.log(circle2,getDiamneter());   // 20
+
+
+
+// <<생성자 함수에 의한 객체 생성 방식의 장점>> 17.2.2
+// 생성자 함수에 의한 객체 생성 방식은 마치 객체(인스턴스)를 생성하기 위한 템플릿(클래스)처럼 생성자 함수를 사용하여 프로퍼티 구조가 동일한 객체 여러 개를 간편하게 생성할 수 있다.
+
+// 생성자 함수
+function Circle(radius) {
+    // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+    this.radius = radius;
+    this.getDiamneter = function () {
+        return 2 * this.radius;
+    };
+}
+
+// 인스턴스의 생성
+const circle1 = new Circle(5);  // 반지름이 5인 Circle 객체를 생성
+const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
+
+console.log(circle1.getDiamneter());
+console.log(circle2.getDiamneter());
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+function Circle(radius) {
+    // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+    this.radius = radius;
+    this.getDiamneter = function () {
+        return 2 * this.radius;
+    };
+}
+
+// 인스턴스의 생성
+const circle1 = new Circle(5);  // 반지름이 5인 Circle 객체를 생성
+const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
+
+console.log(circle1.getDiamneter());
+console.log(circle2.getDiamneter());
